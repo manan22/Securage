2d027dd7fcca9273a3591c60ea0fa8325aa1126d
manan22 <mananpandya@MANANs-MacBook-Pro.local>
2015-01-04 18:25:40 -0500

    Added Files

---
+++ securage_database.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 29, 2014 at 05:39 PM
+-- Server version: 5.5.9
+-- PHP Version: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `securage`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_table`
+--
+
+CREATE TABLE `contact_table` (
+  `phno` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `contact_table`
+--
+
+INSERT INTO `contact_table` VALUES('6477409818');
